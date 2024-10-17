a072d39d67208465453c7845f35b08286511dfdd
asseternity <nakupovasset@gmail.com>
2024-10-17 21:56:59 +0500

    feat: incorporate cloudinary for cloud uploads

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -4,6 +4,7 @@ const multer  = require('multer')
 const path = require('path');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const { v2 } = require('cloudinary'); 
 
 // configure multer's storage
 const storage = multer.diskStorage({
@@ -26,19 +27,26 @@ uploadRoute.get('/', async (req, res, next) => {
     res.render('upload', { user: userWithFolders });
 });
 
-uploadRoute.post('/profile', upload.single('avatar'), async function (req, res, next) {
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
-    if (!req.file) {
-        return res.status(400).json({ error: "No file uploaded." });
-    }
+// configure cloudinary
+v2.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+// cloudinary's upload function
+cloudUpload = async (req, res, next) => {
     const folderId = parseInt(req.body.uploadDBFolder);
     const fileSizeString = String(req.file.size);
+
     try {
+        const result = await v2.uploader.upload(
+            req.file.path
+        )
         await prisma.file.create({
             data: {
                 filename: req.file.filename,
-                filepath: path.join('uploads', req.file.filename),
+                filepath: result.secure_url,
                 filesize: fileSizeString,
                 folderId: folderId,
             }
@@ -49,10 +57,41 @@ uploadRoute.post('/profile', upload.single('avatar'), async function (req, res, 
         });
         res.render('folder', { user: userWithFolders });
     } catch(err) {
-        console.error(err);
-        return res.status(500).json({ error: "An error occurred while saving the file data." })
+        console.log(err)
+        return res.status(500).json({ error: "An error occurred while uploading the file to the cloud." })
     }
-});
+}
+
+uploadRoute.post('/profile', upload.single('avatar'), cloudUpload);
+
+
+// uploadRoute.post('/profile', upload.single('avatar'), async function (req, res, next) {
+//     // req.file is the `avatar` file
+//     // req.body will hold the text fields, if there were any
+//     if (!req.file) {
+//         return res.status(400).json({ error: "No file uploaded." });
+//     }
+//     const folderId = parseInt(req.body.uploadDBFolder);
+//     const fileSizeString = String(req.file.size);
+//     try {
+//         await prisma.file.create({
+//             data: {
+//                 filename: req.file.filename,
+//                 filepath: path.join('uploads', req.file.filename),
+//                 filesize: fileSizeString,
+//                 folderId: folderId,
+//             }
+//         });
+//         const userWithFolders = await prisma.user.findUnique({
+//             where: { id: req.user.id },
+//             include: { folders: { include: { files: true }}}
+//         });
+//         res.render('folder', { user: userWithFolders });
+//     } catch(err) {
+//         console.error(err);
+//         return res.status(500).json({ error: "An error occurred while saving the file data." })
+//     }
+// });
 
 uploadRoute.post('/download/:fileId', async function (req, res, next) {
     const fileId = parseInt(req.params.fileId);
